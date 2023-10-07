a383e97e77513d33cbbf5f42f7f81e9f5ab4c12c
giusencito <66499088+giusencito@users.noreply.github.com>
2023-10-07 17:26:47 -0500

    deploy

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+set -o errexit
+
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
